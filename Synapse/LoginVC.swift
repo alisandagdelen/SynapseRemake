21d81a1958159fe217c86de315d8ee749105c3d0
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-23 23:16:36 +0300

    loginvc created

---
+++ Synapse/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  Synapse
+//
+//  Created by alişan dağdelen on 23.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import Foundation
+
+class LoginVC: BaseVC {
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+    }
+}
