4283817ea129a5eef839bbf01b2c36e203e2cd65
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-24 01:32:33 +0300

    setStyle for custom style

--- Synapse/LoginVC.swift
+++ Synapse/LoginVC.swift
@@ -6,11 +6,26 @@
 //  Copyright © 2017 alisandagdeleb. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class LoginVC: BaseVC {
     
+    @IBOutlet weak var txtFieldPassword: UITextField!
+    @IBOutlet weak var txtFieldLogin: UITextField!
+    
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
+        
+        setStyle()
     }
+    
+    func setStyle() {
+        txtFieldLogin.backgroundColor = UIColor.clear
+        txtFieldPassword.backgroundColor = UIColor.clear
+        
+        let placeholderColor = UIColor.lightText
+        txtFieldLogin.attributedPlaceholder = NSAttributedString(string: "Your e-mail", attributes: [NSForegroundColorAttributeName : placeholderColor])
+        txtFieldPassword.attributedPlaceholder = NSAttributedString(string: "Your password", attributes: [NSForegroundColorAttributeName : placeholderColor])
+    }
+    
 }
