99ec083257f05e38f9d3c1e140424adf4c78d9af
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-31 02:21:47 +0300

    SynapseService created

---
+++ Synapse/SynapseService.swift
@@ -0,0 +1,14 @@
+//
+//  SynapseService.swift
+//  Synapse
+//
+//  Created by alişan dağdelen on 30.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import Foundation
+
+class SynapseService {
+    
+    
+}
