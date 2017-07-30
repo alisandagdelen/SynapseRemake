ab360a1ef1a2743d559d95f158bb91e983b53d36
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-30 17:43:34 +0300

    registerVC created

---
+++ Synapse/RegisterVC.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterVC.swift
+//  Synapse
+//
+//  Created by alişan dağdelen on 26.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: BaseVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func actHideVC(_ sender: UIButton) {
+        hideVC()
+    }
+    
+
+}
