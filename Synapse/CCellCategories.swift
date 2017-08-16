6e89b65859fbda11476d8f57ad531ec149eee753
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-08-17 01:48:45 +0300

    custom collectionView cell for categories

---
+++ Synapse/CCellCategories.swift
@@ -0,0 +1,15 @@
+//
+//  CCellCategories.swift
+//  Synapse
+//
+//  Created by alişan dağdelen on 15.08.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import UIKit
+
+class  CCellCategories: UICollectionViewCell {
+
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgIcon: UIImageView!
+}
